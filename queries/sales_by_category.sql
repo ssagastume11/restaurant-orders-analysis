abe01aab4f7d13c0e880fac152d2644cdbb29c4d
ssagastume11 <sagassergio123@gmail.com>
2025-09-09 09:43:10 -0400

    BigQuery SQL queries

--- queries/sales_by_category.sql
+++ queries/sales_by_category.sql
@@ -1,6 +1,6 @@
 SELECT
   Category,
-  SUM(Quantity) AS Total_Quantity_Sold
+  ROUND(SUM(Quantity * Price), 2) AS Total_Sales
 FROM `plenary-ability-463920-b3.restaurant_dataset.restaurant_orders_cleaned`
 GROUP BY Category
-ORDER BY Total_Quantity_Sold DESC;
+ORDER BY Total_Sales DESC;
